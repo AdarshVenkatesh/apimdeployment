38a7c75faf9e6842732a28b72ee3b9457b647063
Adarsh Venkatesh Bodineni <adarshvenkatesh169@gmail.com>
2023-01-11 16:14:47 -0800

    upd

--- ImportGraphQLApiToApim.ps1
+++ ImportGraphQLApiToApim.ps1
@@ -32,5 +32,5 @@ Invoke-RestMethod -Uri $schemaUrl -OutFile $schemaFileName
 ls
 
 Write-Host "Import GraphQL Api endpoint to apim instance"
-Import-AzApiManagementApi -ServiceUrl $ServiceUrl -ApiType GraphQL -Path $GraphQLSuffix -ApiId $ApiId -Context $apiContext -SpecificationPath .\$schemaFileName -SpecificationUrl $ServiceUrl
+Import-AzApiManagementApi -ServiceUrl $ServiceUrl -ApiType GraphQL -Path $GraphQLSuffix -ApiId $ApiId -Context $apiContext -SpecificationPath .\$schemaFileName -SpecificationFormat GraphQL
 Write-Host "GraphQL APi successfully imported to apim instance.." -ForegroundColor "Green"
